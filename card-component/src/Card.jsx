14c44664798e4e36761becb3fac085dcfc08e11a
a9na <anaiscoding@gmail.com>
2024-08-02 19:12:57 +0200

    commit

--- card-component/src/Card.jsx
+++ card-component/src/Card.jsx
@@ -1,8 +1,11 @@
+import profilePic from './assets/profile.jpg'
+
+
 function Card(){
 
     return (
         <div className="card">
-            <img></img>
+            <img className="card-image" src={profilePic} alt="profile picture"></img>
             <h2>Ana</h2>
             <p>Im studying computer science and love running !</p>
         </div>
