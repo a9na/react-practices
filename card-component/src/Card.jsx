696218bb449e0ef9c1e36787756e7b48d2da1165
a9na <anaiscoding@gmail.com>
2024-08-02 18:59:35 +0200

    commit

--- card-component/src/Card.jsx
+++ card-component/src/Card.jsx
@@ -1,6 +1,12 @@
 function Card(){
 
-
+    return (
+        <div className="card">
+            <img></img>
+            <h2>Ana</h2>
+            <p>Im studying computer science and love running !</p>
+        </div>
+    );
 
 
 }
