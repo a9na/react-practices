cbdb8e9a5ab0a98b9102848cf44140c0849f971a
a9na <anaiscoding@gmail.com>
2024-08-04 18:44:07 +0200

    commit

--- render-lists/src/List.jsx
+++ render-lists/src/List.jsx
@@ -1,6 +1,9 @@
 
 
-function List(){
+function List(props){
+
+    const category = props.category;
+    const itemList = props.items;
 
     /*
     const fruits = ["apple", "orange", "banana", "coconut", "pineapple"];
@@ -12,12 +15,12 @@ function List(){
     return(<ul>{listItems}</ul>);
 */
 
-    const fruits = [{id: 1, name: "apple", calories: 95}, 
+ /*   const fruits = [{id: 1, name: "apple", calories: 95}, 
                     {id: 2, name:"orange", calories: 45}, 
                     {id: 3, name: "banana", calories: 105}, 
                     {id: 4, name: "coconut", calories: 159}, 
                     {id: 5, name: "pineapple", calories: 37}];
-
+*/
     //fruits.sort((a, b) => a.name.localeCompare(b.name));  //ALPHABETICAL
     //fruits.sort((a, b) => b.name.localeCompare(a.name));  //REVERSE ALPHABETICAL
     //fruits.sort((a, b) => a.calories - b.calories); //NUMERIC 
@@ -31,21 +34,21 @@ function List(){
 
     //fruits.sort((a, b) => b.calories - a.calories); //REVERSE NUMERIC (DESCENDING)
 
-    const lowCalFruits = fruits.filter(fruit.calories < 100);
+    //const lowCalFruits = fruits.filter(fruit.calories < 100);
 
     //const listItems = fruits.map(fruit => <li key={fruit.id}>{fruit.name}: &nbsp; <b>{fruit.calories}</b></li>);
 
+    const listItems = itemList.map(item => <li key={item.id}>{item.name}: &nbsp; <b>{item.calories}</b></li>);
 
-    const highCalFruits = fruits.filter(fruit.calories >= 100);
+    //const highCalFruits = fruits.filter(fruit.calories >= 100);
+
 
     //const listItems = lowCalFruits.map(lowCalFruit => <li key={lowCalFruit.id}>{lowCalFruit.name}: &nbsp; <b>{lowCalFruit.calories}</b></li>);
 
-    const listItems = highCalFruits.map(highCalFruit => <li key={highCalFruit.id}>{highCalFruit.name}: &nbsp; <b>{highCalFruit.calories}</b></li>);
+    //const listItems = highCalFruits.map(highCalFruit => <li key={highCalFruit.id}>{highCalFruit.name}: &nbsp; <b>{highCalFruit.calories}</b></li>);
 
 
-    return(<ul>{listItems}</ul>);
-
-
+    return(<><h3 className="list-category">{category}</h3><ol className="list-items">{listItems}</ol></>);
 
 
 }
