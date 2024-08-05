f0ab194f9082b9e17dc2268ae8beb2f15dec38c1
a9na <anaiscoding@gmail.com>
2024-08-05 23:35:23 +0200

    commit

--- use-state/src/MyComponent.jsx
+++ use-state/src/MyComponent.jsx
@@ -2,8 +2,19 @@ import React, {useState} from 'react'
 
 function MyComponent(){
 
+    const [name, setName] = useState();
+
+    const updateName = () => {
+        name = "Ana";
+        console.log(name);
+    }
 
 
+    return( <div>
+                <p>Name: {name}</p>
+                <button onClick={updateName}>Set Name</button>
+
+            </div>)
 }
 
 
