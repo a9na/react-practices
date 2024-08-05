a8a83b0acbcb7db291152f7988faad59f0a84e40
a9na <anaiscoding@gmail.com>
2024-08-05 13:06:51 +0200

    commit

--- click-events/src/Button.jsx
+++ click-events/src/Button.jsx
@@ -2,8 +2,29 @@
 
 function Button(){
 
+    let count = 0;
 
-    return(<button></button>)
+    //const handleClick = () => console.log("OUCH!")
+
+    const handleClick = (name) => {
+        if(count < 3){
+            count++;
+            console.log(`${name} you clicked me ${count} time/s`)
+        }
+        else{
+            console.log(`${name} stop clicking me!`)
+        }
+    }
+
+    //const handleClick2 = (name) => console.log(`${name} stop clicking me`)
+
+
+    //return(<button onClick={handleClick2("Ana")}>Click me </button>)
+    // if you add a set of parentheses after a callback, you'll invoke it right away
+
+    //return(<button onClick={() => handleClick2("Ana")}>Click me </button>)
+
+    return(<button onClick={handleClick}>Click me </button>)
 }
 
 
