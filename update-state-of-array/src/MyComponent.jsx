518827c9e6cb1ef3fa4a5359e5e46b80f978f45b
a9na <anaiscoding@gmail.com>
2024-08-06 22:50:28 +0200

    commit

--- update-state-of-array/src/MyComponent.jsx
+++ update-state-of-array/src/MyComponent.jsx
@@ -5,6 +5,23 @@ function MyComponent() {
 
     const [foods, setFoods] = useState(["Apple", "Orange", "Banana"]);
 
+    function handleAddFood(){
+        const newFood = document.getElementById("foodInput").value;
+    }
+
+    function handleremoveFood() {
+
+    }
+
+    return( <div>
+                <h2>List of Food</h2>
+                <ul>
+                    {foods.map((food, index) => <li key={index}>{food}</li>)}
+                </ul>
+                <input type = "text" id="foodInput" placeholder='Enter food name'/>
+                <button onClick={handleAddFood}>Add Food</button>
+            </div>);
+
 }
 
 
