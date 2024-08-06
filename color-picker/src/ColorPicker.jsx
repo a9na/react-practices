f85572a6c9821e660e75f791d9eab29b1bbb737b
a9na <anaiscoding@gmail.com>
2024-08-06 16:56:06 +0200

    commit

--- color-picker/src/ColorPicker.jsx
+++ color-picker/src/ColorPicker.jsx
@@ -4,7 +4,9 @@ function ColorPicker(){
 
     const [color, setColor] = useState("#FFFFFF");
 
-
+    function handleColorChange(event) {
+        setColor(event.target.value);
+    }
 
     return( <div className='color-picker-container'>
                 <h1>Color Picker</h1>
@@ -12,7 +14,7 @@ function ColorPicker(){
                     <p>Selected Color: {color}</p>
                 </div>
                 <label>Select a color: </label>
-                <input type="color"/>
+                <input type="color" value={color} onChange={handleColorChange}/>
             </div>)
 }
 
