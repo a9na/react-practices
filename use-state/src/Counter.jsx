07e28aec4c7e05ea2931418e50a5c5d719bb52ea
a9na <anaiscoding@gmail.com>
2024-08-06 14:08:48 +0200

    commit

--- use-state/src/Counter.jsx
+++ use-state/src/Counter.jsx
@@ -19,8 +19,9 @@ function Counter() {
 
     return ( <div className='counter-container'>
                 <p className='count-display'>{count}</p>
-                <button className='counter-button' onClick={decrement}></button>
-
+                <button className='counter-button' onClick={decrement}>Decrement</button>
+                <button className='counter-button' onClick={reset}>Reset</button>
+                <button className='counter-button' onClick={increment}>Increment</button>
             </div>)
 
 }
