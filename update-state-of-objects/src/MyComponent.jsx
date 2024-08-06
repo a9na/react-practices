c188992f441c87df2d0c3910c0e7446f09b49c3d
a9na <anaiscoding@gmail.com>
2024-08-06 17:18:18 +0200

    commit

--- update-state-of-objects/src/MyComponent.jsx
+++ update-state-of-objects/src/MyComponent.jsx
@@ -7,9 +7,23 @@ function MyComponent() {
                                     make: "Ford", 
                                     model: "Mustang"});
 
+    function handleYearChange(event){
+        setCar({...car, year: 2025});
+    }
+
+    function handleMakeChange(event){
+        
+    }
+
+    function handleModelChange(event){
+        
+    }
+
     return (<div>
         <p>Your favorite car is: {car.year} {car.make} {car.model}</p>
-        <input type="number" value={car.year}/>
+        <input type="number" value={car.year} onChange={handleYearChange}/> <br/>
+        <input type="text" value={car.make} onChange={handleMakeChange}/> <br/>
+        <input type="text" value={car.model} onChange={handleModelChange}/> <br/>
     </div>)
 
 }
