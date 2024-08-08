de4a20aee210815fb51240d23f40f3e6ee24a5e1
a9na <anaiscoding@gmail.com>
2024-08-08 12:05:42 +0200

    commit

---
+++ use-effect/src/App.jsx
@@ -0,0 +1,22 @@
+/*
+REACT HOOK= special function that allows functional
+components to use React features without writing class 
+components
+
+useState()= a react hook that allows the creation
+of a stateful variable AND a setter function to
+update its value in the Virtual DOM [name, setName]
+*/
+
+import MyComponent from './MyComponent.jsx'
+
+
+function App() {
+  
+
+  return(<MyComponent></MyComponent>
+  );
+
+}
+
+export default App
---
+++ use-effect/src/MyComponent.jsx
@@ -0,0 +1,9 @@
+import React, {useState} from 'react'
+
+function MyComponent(){
+
+    return(<></>)
+}
+
+
+export default MyComponent
