25709edbd81b38d1fa6b2ceaaed8e880ddf35dd2
a9na <anaiscoding@gmail.com>
2024-08-09 17:08:35 +0200

    commit

--- use-effect-2/src/MyComponent.jsx
+++ use-effect-2/src/MyComponent.jsx
@@ -5,7 +5,24 @@ function MyComponent(){
     const [width, setWidth] = useState(window.innerWidth);
     const [height, setHeight] = useState(window.innerHeight);
 
+    useEffect(() => {
+        window.addEventListener("resize", handleResize);
 
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        }
+    }, []);
+
+    //window.addEventListener("resize", handleResize);
+
+    useEffect(() => {
+        document.title = `Size: ${width} x ${height}`;
+    }, [width, height]);
+
+    function handleResize(){
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);
+    }
 
 
     return(<>
