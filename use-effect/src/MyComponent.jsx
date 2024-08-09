a2b44913b94e22528df78a035bab46b93d2b1ef8
a9na <anaiscoding@gmail.com>
2024-08-09 14:06:50 +0200

    commit

--- use-effect/src/MyComponent.jsx
+++ use-effect/src/MyComponent.jsx
@@ -4,7 +4,19 @@ function MyComponent(){
 
     const [count, setCount] = useState(0);
 
-    return(<></>)
+    useEffect(() => {
+        document.title = // `Count: ${count}`
+        `My Counter Program`; 
+    }, []);  //title only updates once
+
+    function addCount(){
+        setCount(prevCount => prevCount + 1);
+    }
+
+    return(<>
+        <p>Count: {count}</p>
+        <button onClick={addCount}>Add</button>
+    </>)
 }
 
 
